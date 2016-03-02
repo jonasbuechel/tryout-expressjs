cda1408d71f5ee7e01be8dd9cdb3232aa2ff0e4d
Jonas Büchel <Jonas Bchel>
2016-03-02 06:23:53 +0100

    add a secound route

--- src/app.js
+++ src/app.js
@@ -7,8 +7,14 @@ var serverPort  = 3000;
 
 var app = express();
 
+//ROUTE HOME "/"
+app.get('/', function(req, res){
+    res.send("<h1>HOME</h1><p>This is my home"); 
+});
+
+//ROUTE TEST "/test"
 app.get('/test', function(req, res){
-    res.send("<h1>This is my first response!</h1><p>I'ts containing already html :)"); 
+    res.send("<h1>This is my testpage!</h1><p>I'ts containing already html :)"); 
 });
 
 app.listen(serverPort, function(){
