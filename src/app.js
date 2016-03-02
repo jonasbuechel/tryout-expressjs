e6946f121212b6a2899488736c6dd6bb94391e98
Jonas Büchel <Jonas Bchel>
2016-03-02 06:31:33 +0100

    blog : added some mock-data as json which is served at the blog page

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 'use strict';
 
 var express     = require('express');
+var blogPosts   = require('./mockdata/posts.json');
 var serverPort  = 3000; 
 
 var app = express();
@@ -12,9 +13,9 @@ app.get('/', function(req, res){
     res.send("<h1>HOME</h1><p>This is my home"); 
 });
 
-//ROUTE TEST "/test"
-app.get('/test', function(req, res){
-    res.send("<h1>This is my testpage!</h1><p>I'ts containing already html :)"); 
+//ROUTE BLOG "/blog"
+app.get('/blog', function(req, res){
+    res.send(blogPosts); 
 });
 
 app.listen(serverPort, function(){
