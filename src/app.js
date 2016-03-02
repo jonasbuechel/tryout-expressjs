69cb3547b13df41a5a6801962bfcbbaee9810a25
Jonas Büchel <Jonas Bchel>
2016-03-02 06:07:05 +0100

    add minimal express-server

--- src/app.js
+++ src/app.js
@@ -2,4 +2,15 @@
 
 'use strict';
 
-var express = require(express);
+var express     = require('express');
+var serverPort  = 3000; 
+
+var app = express();
+
+app.get('/test', function(request, response){
+    response.send('This is my first response!'); 
+});
+
+app.listen(serverPort, function(){
+    console.log('express server startet at localhost:'+serverPort);
+});
